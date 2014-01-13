3dc51f9ac664c3475bf7ab2c95b266ee437a0aa7
Ziya SARIKAYA <sarikayaziya@gmail.com>
2014-01-12 19:24:16 -0800

    Performed renaming

---
+++ src/Autofac.Extras.NLog.Tests/SampleClassWithConstructorDependency.cs
@@ -0,0 +1,24 @@
+﻿using NLog;
+
+namespace Autofac.Extras.NLog.Tests
+{
+    public class SampleClassWithConstructorDependency : ISampleClass
+    {
+        private readonly ILogger _logger;
+
+        public SampleClassWithConstructorDependency(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void SampleMessage(string message)
+        {
+            _logger.Debug(message);
+        }
+
+        public ILogger GetLogger()
+        {
+            return _logger;
+        }
+    }
+}
---
+++ src/Autofac.Extras.NLog.Tests/SampleClassWithPropertyDependency.cs
@@ -0,0 +1,17 @@
+﻿namespace Autofac.Extras.NLog.Tests
+{
+    public class SampleClassWithPropertyDependency : ISampleClass
+    {
+        public ILogger Logger { get; set; }
+
+        public void SampleMessage(string message)
+        {
+            Logger.Debug(message);
+        }
+
+        public ILogger GetLogger()
+        {
+            return Logger;
+        }
+    }
+}
