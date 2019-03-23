6da54951bd64e50eec267c92e858600e622b41e8
Craig Boland <craig.boland@gmail.com>
2019-03-22 21:53:04 -0500

    Prepare for release 2.0.0 - Upgrade project type to simplify multi-targeting. - Upgrade NLog to v4.5 to allow targeting .NET Standard.

--- src/Autofac.Extras.NLog/Properties/AssemblyInfo.cs
+++ src/Autofac.Extras.NLog/Properties/AssemblyInfo.cs
@@ -4,12 +4,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Autofac.Extras.NLog")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Autofac.Extras.NLog")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,16 +14,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1db81670-1fb6-43c6-94db-4b0567effdd3")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
