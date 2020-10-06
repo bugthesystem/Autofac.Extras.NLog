c6f8450077fe556c5333a6224b0815974310474a
Craig Boland <cboland@alliedpilots.org>
2020-10-06 17:38:36 -0500

    Updated module and added middleware for Autofac 6.0 resolve pipeline.

---
+++ src/Autofac.Extras.NLog/NLogMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using Autofac.Core;
+using Autofac.Core.Resolving.Pipeline;
+using NLog;
+using System.Linq;
+using System.Reflection;
+
+// Adapted from: log4net Integration Middleware
+// https://autofaccn.readthedocs.io/en/latest/examples/log4net.html#log4net-integration-middleware
+
+namespace Autofac.Extras.NLog
+{
+    internal class NLogMiddleware : IResolveMiddleware
+    {
+        public PipelinePhase Phase => PipelinePhase.ParameterSelection;
+
+        public void Execute(ResolveRequestContext context, System.Action<ResolveRequestContext> next)
+        {
+            // Add our parameters.
+            context.ChangeParameters(context.Parameters.Union(
+                new[]
+                {
+                    new ResolvedParameter(
+                        (p, i) => p.ParameterType == typeof (ILogger),
+                        (p, i) => LogManager.GetLogger(p.Member.DeclaringType.FullName))
+                }));
+
+            // Continue the resolve.
+            next(context);
+
+            // Has an instance been activated?
+            if (context.NewInstanceActivated)
+            {
+                var instanceType = context.Instance.GetType();
+
+                // Get all the injectable properties to set.
+                // If you wanted to ensure the properties were only UNSET properties,
+                // here's where you'd do it.
+                var properties = instanceType
+                    .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                    .Where(p => p.PropertyType == typeof(ILogger) && p.CanWrite && p.GetIndexParameters().Length == 0);
+
+                // Set the properties located.
+                foreach (var propToSet in properties)
+                {
+                    propToSet.SetValue(context.Instance, LogManager.GetLogger(instanceType.FullName), null);
+                }
+            }
+        }
+    }
+}
--- src/Autofac.Extras.NLog/NLogModule.cs
+++ src/Autofac.Extras.NLog/NLogModule.cs
@@ -1,49 +1,29 @@
 ﻿using Autofac.Core;
 using Autofac.Core.Registration;
-using NLog;
-using System.Linq;
-using System.Reflection;
+using Autofac.Core.Resolving.Pipeline;
+
+// Adapted from: log4net Integration Middleware
+// https://autofaccn.readthedocs.io/en/latest/examples/log4net.html#log4net-integration-middleware
 
 namespace Autofac.Extras.NLog
 {
     public class NLogModule : Module
     {
-        private static void InjectLoggerProperties(object instance)
+        private readonly IResolveMiddleware middleware;
+
+        public NLogModule()
         {
-            var instanceType = instance.GetType();
-
-            // Get all the injectable properties to set.
-            // If you wanted to ensure the properties were only UNSET properties,
-            // here's where you'd do it.
-            var properties = instanceType
-              .GetProperties(BindingFlags.Public | BindingFlags.Instance)
-              .Where(p => p.PropertyType == typeof(ILogger) && p.CanWrite && p.GetIndexParameters().Length == 0);
-
-            // Set the properties located.
-            foreach (var propToSet in properties)
-            {
-                propToSet.SetValue(instance, LogManager.GetLogger(instanceType.FullName), null);
-            }
-        }
-
-        private static void OnComponentPreparing(object sender, PreparingEventArgs e)
-        {
-            e.Parameters = e.Parameters.Union(
-                new[]
-                {
-                    new ResolvedParameter(
-                        (p, i) => p.ParameterType == typeof (ILogger),
-                        (p, i) => LogManager.GetLogger(p.Member.DeclaringType.FullName))
-                });
+            middleware = new NLogMiddleware();
         }
 
         protected override void AttachToComponentRegistration(IComponentRegistryBuilder componentRegistry, IComponentRegistration registration)
         {
-            // Handle constructor parameters.
-            registration.Preparing += OnComponentPreparing;
-
-            // Handle properties.
-            registration.Activated += (sender, e) => InjectLoggerProperties(e.Instance);
+            // Attach to the registration's pipeline build.
+            registration.PipelineBuilding += (sender, pipeline) =>
+            {
+                // Add our middleware to the pipeline.
+                pipeline.Use(middleware);
+            };
         }
     }
 }
